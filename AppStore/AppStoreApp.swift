b4c4dc022d2391ffed4bd21ddf8c25dee7df4484
Baris Karalar <karalarbaris@gmail.com>
2024-09-26 19:14:28 +0200

    Initial Commit

---
+++ AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by Baris Karalar on 26.09.24.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
