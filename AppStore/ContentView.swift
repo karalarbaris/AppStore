6199f6829dd5e59cbaeb3aa5caaa7d4424aaf15d
Baris Karalar <karalarbaris@gmail.com>
2024-10-02 11:06:58 +0200

    Rendering views with ScrollView and GeometryReader

--- AppStore/ContentView.swift
+++ AppStore/ContentView.swift
@@ -9,16 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            GeometryReader { proxy in
+                ScrollView {
+                    ForEach(0..<10) { num in
+                        VStack(spacing: 16) {
+                            AppIconTitleView()
+                            
+                            ScreenshotsRow(proxy: proxy)
+                        }
+                    }
+                    .padding(16)
+                }
+            }
+            .navigationTitle("Search")
+            .searchable(text: .constant("Enter search term"))
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
+}
+
+struct ScreenshotsRow: View {
+    
+    let proxy: GeometryProxy
+    
+    var body: some View {
+        let width = (proxy.size.width - 4 * 16) / 3
+        HStack(spacing: 16) {
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: width, height: 200)
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: width, height: 200)
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: width, height: 200)
+        }
+    }
+}
+
+struct AppIconTitleView: View {
+    var body: some View {
+        HStack(spacing: 16) {
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: 80, height: 80)
+            VStack(alignment: .leading) {
+                Text("Youtube: Watch listen etc etc etc")
+                    .lineLimit(1)
+                    .font(.system(size: 20))
+                Text("Photo & Video")
+                    .foregroundStyle(.gray)
+                Text("Stars 30.0M")
+            }
+            Image(systemName: "icloud.and.arrow.down")
+        }
+    }
 }
